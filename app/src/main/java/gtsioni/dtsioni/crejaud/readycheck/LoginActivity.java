133ad6c9d01d0835fd319aa5582c86ef0dc035db
Daniel Tsioni - LP <dtsioni@gmail.com>
2015-06-07 14:45:57 -0400

    Removed action bar from log in view

--- app/src/main/java/gtsioni/dtsioni/crejaud/readycheck/LoginActivity.java
+++ app/src/main/java/gtsioni/dtsioni/crejaud/readycheck/LoginActivity.java
@@ -2,10 +2,16 @@ package gtsioni.dtsioni.crejaud.readycheck;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setContentView(R.layout.activity_login.xml);
     }
 }
